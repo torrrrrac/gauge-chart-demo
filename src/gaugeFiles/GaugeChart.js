71320773a4a7b352378cfacaeee75af4b936f4f1
Swagat Nayak <msndipapal@gmail.com>
2024-11-11 07:08:45 +0530

    Update GaugeChart.js

--- src/gaugeFiles/GaugeChart.js
+++ src/gaugeFiles/GaugeChart.js
@@ -28,11 +28,11 @@ const GaugeChart = () => {
   useEffect(() => {
     const updateDimensions = () => {
       if (containerRef.current) {
-        const containerWidth = containerRef.current.offsetWidth;
-        const chartWidth = Math.min(containerWidth, 500); // Limit maximum width
+        const width = containerRef.current.offsetWidth;
+        // Increase height to accommodate the value display
         setDimensions({
-          width: chartWidth,
-          height: (chartWidth / 2) + 80 // Maintain aspect ratio plus space for value
+          width: width,
+          height: width / 2 + 80, // Added extra space for the value display
         });
       }
     };
@@ -77,7 +77,7 @@ const GaugeChart = () => {
   // Calculate dimensions based on container size
   const { width, height } = dimensions;
   const centerX = width / 2;
-  const centerY = height - 80;
+  const centerY = height - 50; // Adjust center point to make room for value display
   const radius = Math.min(width / 2, (height - 80) / 1.2);
 
   // Define labels with positions
@@ -95,115 +95,145 @@ const GaugeChart = () => {
         <div className="gauge-content">
           <div ref={containerRef} className="gauge-chart-container">
             {width > 0 && (
-              <>
-                <PieChart width={width} height={height}>
-                  <Pie
-                    data={backgroundData}
-                    cx={centerX}
-                    cy={centerY}
-                    startAngle={180}
-                    endAngle={0}
-                    innerRadius={radius - 30}
-                    outerRadius={radius + 20}
-                    fill="var(--gauge-background)"
-                    stroke="none"
-                    dataKey="value"
-                    cornerRadius={10}
-                  />
+              <PieChart width={width} height={height}>
+                <Pie
+                  data={backgroundData}
+                  cx={centerX}
+                  cy={centerY}
+                  startAngle={180}
+                  endAngle={0}
+                  innerRadius={radius - 30}
+                  outerRadius={radius + 20}
+                  fill="var(--gauge-background)"
+                  stroke="none"
+                  dataKey="value"
+                  cornerRadius={5}
+                />
 
-                  <Pie
-                    data={valueData}
-                    cx={centerX}
-                    cy={centerY}
-                    startAngle={180}
-                    endAngle={180 - angle}
-                    innerRadius={radius - 30}
-                    outerRadius={radius + 20}
-                    fill="url(#gaugeGradient)"
-                    stroke="none"
-                    dataKey="value"
-                    cornerRadius={10}
-                  />
+                <Pie
+                  data={valueData}
+                  cx={centerX}
+                  cy={centerY}
+                  startAngle={180}
+                  endAngle={180 - angle}
+                  innerRadius={radius - 90}
+                  outerRadius={radius + 20}
+                  fill="#090909"
+                  stroke="none"
+                  dataKey="value"
+                  cornerRadius={2}
+                />
 
-                  <defs>
-                    <linearGradient id="gaugeGradient" x1="0" y1="0" x2="1" y2="0">
-                      <stop offset="0%" stopColor="var(--gauge-gradient-start)" />
-                      <stop offset="100%" stopColor="var(--gauge-gradient-end)" />
-                    </linearGradient>
-                  </defs>
+                <Pie
+                  data={valueData}
+                  cx={centerX}
+                  cy={centerY}
+                  startAngle={180}
+                  endAngle={180 - angle}
+                  innerRadius={radius - 30}
+                  outerRadius={radius + 20}
+                  fill="url(#gaugeGradient)"
+                  stroke="none"
+                  dataKey="value"
+                  cornerRadius={2}
+                />
 
-                  {labels.map((label) => {
-                    const labelX =
-                      centerX +
-                      label.radius * Math.cos((label.angle * Math.PI) / 180);
-                    const labelY =
-                      centerY +
-                      label.radius * Math.sin((label.angle * Math.PI) / 180);
-                    const lineEndX =
-                      centerX +
-                      (radius - 75) * Math.cos((label.angle * Math.PI) / 180);
-                    const lineEndY =
-                      centerY +
-                      (radius - 75) * Math.sin((label.angle * Math.PI) / 180);
+                {/* <Pie
+                  data={valueData}
+                  cx={centerX}
+                  cy={centerY}
+                  startAngle={180 - angle + 0.5}
+                  endAngle={180 - angle + 0.5}
+                  innerRadius={radius - 40}
+                  outerRadius={radius + 40}
+                  fill="var(--gauge-text-highlight)"
+                  stroke="none"
+                  dataKey="value"
+                /> */}
 
-                    return (
-                      <g key={label.value}>
-                        <line
-                          x1={centerX}
-                          y1={centerY}
-                          x2={lineEndX}
-                          y2={lineEndY}
-                          stroke="var(--gauge-grid-line)"
-                          strokeWidth={0.5}
-                        />
-                        <text
-                          x={labelX}
-                          y={labelY}
-                          fill="var(--gauge-text)"
-                          fontSize="14"
-                          fontFamily="Space Grotesk"
-                          fontWeight="500"
-                          textAnchor="middle"
-                          dominantBaseline="middle"
-                        >
-                          {label.value}
-                        </text>
-                      </g>
-                    );
-                  })}
+                <defs>
+                  <linearGradient
+                    id="gaugeGradient"
+                    x1="0"
+                    y1="0"
+                    x2="1"
+                    y2="0"
+                  >
+                    <stop offset="0%" stopColor="var(--gauge-gradient-start)" />
+                    <stop offset="100%" stopColor="var(--gauge-gradient-end)" />
+                  </linearGradient>
+                </defs>
 
-                  <circle
-                    cx={centerX}
-                    cy={centerY}
-                    r={4}
-                    fill="var(--gauge-center-point)"
-                  />
-                </PieChart>
+                {labels.map((label) => {
+                  const labelX =
+                    centerX +
+                    (label.radius - 20) *
+                      Math.cos((label.angle * Math.PI) / 180);
+                  const labelY =
+                    centerY +
+                    (label.radius - 20) *
+                      Math.sin((label.angle * Math.PI) / 180);
 
+                  return (
+                    <g key={label.value}>
+                      <text
+                        x={labelX}
+                        y={labelY}
+                        fill="var(--gauge-label-text)"
+                        fontSize="14"
+                        fontFamily="Space Grotesk"
+                        fontWeight="500"
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        {label.value}
+                      </text>
+                    </g>
+                  );
+                })}
+              </PieChart>
+            )}
+
+            {width > 0 && (
+              <div
+                className="gauge-needle"
+                style={{
+                  top: `${centerY}px`,
+                  left: `${centerX}px`,
+                  transform: `translate(-50%, -50%) rotate(${angle - 90}deg)`,
+                }}
+              >
+                <img src="/needle.svg" alt="gauge needle" />
                 <div
-                  className="gauge-needle"
+                  className="value-display"
                   style={{
-                    top: `${centerY}px`,
-                    left: `${centerX}px`,
-                    transform: `translate(-50%, -50%) rotate(${angle - 90}deg)`,
+                    color: `var(--gauge-text-highlight)`,
+                    position: "absolute",
+                    top: `-${radius + 50}px`, // Adjust this value to position the display properly
+                    left: "50%",
+                    transform: `translateX(-50%)`,
+                    fontSize: "clamp(20px, 5vw, 28px)",
+                    fontWeight: "700",
                   }}
                 >
-                  <img src="/needle.svg" alt="gauge needle" />
-                  <div
-                    className="value-display"
-                    style={{
-                      position: "absolute",
-                      top: "-120px",
-                      left: "50%",
-                      transform: "translateX(-50%)",
-                      fontSize: "clamp(20px, 5vw, 28px)",
-                      fontWeight: "700",
-                    }}
-                  >
-                    {gaugeValue}%
-                  </div>
+                  {gaugeValue}%
                 </div>
-              </>
+                <div
+                  className="line-display"
+                  style={{
+                    width: 2,
+                    height: `65px`,
+                    backgroundColor: `var(--gauge-text)`,
+                    position: "absolute",
+                    top: `-${radius }px`, // Adjust this value to position the display properly
+                    left: "50%",
+                    transform: `translateX(-50%)`,
+                    fontSize: "clamp(20px, 5vw, 28px)",
+                    fontWeight: "700",
+                    
+                  }}
+                />
+              </div>
             )}
           </div>
 
